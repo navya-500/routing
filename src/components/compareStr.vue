d84cca2f4c0947baf0d2b2f3a87c9273b598db5c
navya-500 <navya.doddi@500apps.com>
2022-04-07 19:53:57 +0530

    Task(07-04-2022)

--- src/components/compareStr.vue
+++ src/components/compareStr.vue
@@ -8,7 +8,8 @@ export default{
     name:'CompareString',
     data(){
         return{
-            arr:["Hardworking","COmMitment","Smart","dedicated"],
+            arr: "Hardworking commitment smart dedicated",
+            //arr:["Hardworking","COmMitment","Smart","dedicated"],
             str:"Iam Hardworking and DEDICATED employee.Iam sMaRT in Written code and commitment",
             res : 'success'
 
@@ -16,13 +17,16 @@ export default{
     },
     methods:{
         compare(){
-          
+            this.arr=this.arr.split(" ");
+            Array.from(this.arr);
+            
             for(var i=0;i<this.arr.length;i++){
             if(this.str.toUpperCase().includes(this.arr[i].toUpperCase(),this.arr.length)){
             //document.write("success"+"<br>")
     
             }
             else{
+            //document.write("fail"+"<br>")
             this.res='fail'
             }
         
---
+++ src/components/fetch.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+</div>
+</template>
+<script>
+var axios = require("axios").default;
+export default {
+name: 'fetcH',
+components: {},
+async mounted(){
+  let response=await this.getData()
+    console.log(response)
+            // Hit API and fetch data through function var response = getData();
+            },
+methods: {
+    getData(){
+let response= new Promise((resolve, reject) => {         
+// Using axios to fetch API URL         
+axios.get("http://myjson.dit.upm.es/api/bins/fov1").then((response) => {                
+ // resolve when response is received                 
+ resolve(response.data);          
+    }).catch((error) => {         
+            // reject when error is occured                 
+            reject(error);
+            }); 
+            });
+   response.then((data) => {console.log("response", data) }).catch((err) => {console.log(err)})  
+}
+// let mypromise= new Promise((resolve,reject)=>{
+// setTimeout(()=>{
+//     resolve(586)
+// },5000)
+    
+// })
+}
+}
+</script>
