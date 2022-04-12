7df56f4e95f0e44291f583e39c74e999de85d37e
navya-500 <navya.doddi@500apps.com>
2022-04-12 19:09:34 +0530

    Assignment-02

---
+++ src/components/Assignment02/QUE4(a).vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    components Registration in local and global
+        <h4>message:{{message}}</h4>
+        <h4>name:{{name}}</h4>
+        <h4>number:{{number}}</h4>  
+        <h4>Items:{{Items}}</h4>
+        <i><b><h3>mantra:{{mantra}}</h3></b></i>
+    </div>
+</template>
+<script>
+export default {
+    name : "compReg",
+
+props : {
+    message: String,
+    name : String,
+    number : 
+     {
+        default:1138,
+        type:Number
+    },
+    mantra: String,
+Items:[
+      {age:20,firstname:"navya",lastname:"doddi"},
+      {age:24,firstname:"divya",lastname:"lagadapati"}
+    ]
+    },
+methods:{
+    fun(){
+        alert("iam child")
+    }
+}
+
+
+}
+</script>
---
+++ src/components/Assignment02/QUE4(b).vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+  
+  <que4a :message="message" :name="name" ref="refg" :number="number" :mantra="mantra"></que4a>
+  <que4a :message="message" :name="name" ref="navya" :number="number" :Items="Items"></que4a>
+
+    hello world
+  <button @click="click()">submit</button>
+  
+
+  </div>
+</template>
+
+<script>
+import que4a from '../components/QUE4(a)'
+export default {
+  name: 'HelloWorld',
+  //local declaration in que-3
+  components: {
+    
+    que4a,  
+  },
+  data(){
+    return{
+      message:"vue components",
+      name:"navya",
+    
+      Items:[
+      {age:20,firstname:"navya",lastname:"doddi"},
+      {age:24,firstname:"divya",lastname:"lagadapati"}
+      ],
+      mantra:"mantra technologies"
+    }
+  },
+  methods:{
+    click(){
+      this.$refs.refg.fun();
+      this.$refs.navya.Items;
+      alert("i did it")
+    }
+  }
+}
+</script>
