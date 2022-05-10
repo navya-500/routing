e5494c189eff2b370cbeecaa4676d42dd0784c70
navya-500 <navya.doddi@500apps.com>
2022-05-10 17:33:09 +0530

    test1 (10-05-2022)

---
+++ src/components/test-KVS/quest05.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-table
+      id="table-transition-example"
+      :items="items"
+      :fields="fields"
+      striped
+      small
+      primary-key="a"
+      :tbody-transition-props="transProps"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        transProps: {
+          name: 'flip-list'
+        },
+        items: [
+          { productname: "pendrive", price: '300', productcategory: '' },
+          { productname: "laptop", price: '30000', productcategory: 'product category' },
+        ],
+        fields: [
+          { key: 'productname', sortable: true },
+          { key: 'price', sortable: true },
+          { key: 'productcategory', sortable: true }
+        ]
+      }
+    }
+  }
+</script>
